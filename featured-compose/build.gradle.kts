68034121dc6201a9c35bb1f24c5d006287fa4b91
krozov <kirill.rozov@alfa-bank.by>
2026-03-19 12:39:08 +0300

    feat: scaffold featured-compose Android-only module

---
+++ featured-compose/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+android {
+    namespace = "dev.androidbroadcast.featured.compose"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+kotlin {
+    explicitApi()
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(compose.runtime)
+}
